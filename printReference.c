b2d479005a1b72898594a3b29dd21850c55d3939
Yash <yash.parsai@in.bosch.com>
2022-06-13 07:19:48 -0400

    adding new files

---
+++ printReference.c
@@ -0,0 +1,27 @@
+#include "colorCoding.h"
+
+extern int numberOfMajorColors;
+extern int numberOfMinorColors;
+
+void printReference(void)
+{
+
+int majorCount = 0;
+int minorCount = 0;
+ColorPair colorPair;
+
+printf("==================color coding reference manual====================\n");
+printf("Pair No.\t\tmajor color\t\tminor color\n");
+for(; majorCount<=numberOfMajorColors; majorCount++)
+{
+   colorPair.majorColor = majorCount;
+   for(; minorCount<=numberOfMinorColors; minorCount++)
+   {
+      colorPair.minorColor = minorCount;
+      printf("%d\t\t",GetPairNumberFromColor(&colorPair));
+      printf("%s\t\t",MajorColorNames[majorCount]);
+      printf("%s\t\t\n",MinorColorNames[minorCount]);
+
+   }
+}
+}
