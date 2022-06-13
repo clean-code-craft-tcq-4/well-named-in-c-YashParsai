4a8ad7d784963771b0cf3dffc76fbcf0b50657c4
YashParsai <105203897+YashParsai@users.noreply.github.com>
2022-06-13 17:45:32 +0530

    Update colorCoding.h

--- colorCoding.h
+++ colorCoding.h
@@ -3,7 +3,8 @@
 enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET};
 enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
 
-typedef struct {
+typedef struct 
+{
     enum MajorColor majorColor;
     enum MinorColor minorColor;
 } ColorPair;
