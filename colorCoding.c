53a548b3b42cdaebc85d64838bfd486d1fedc91c
Yash <yash.parsai@in.bosch.com>
2022-06-13 06:28:59 -0400

    Added new print reference manual funciton for colr coding

--- colorCoding.c
+++ colorCoding.c
@@ -3,17 +3,11 @@
 
 /************Variable declerations********************/
 
-const char* MajorColorNames[] = {
-    "White", "Red", "Black", "Yellow", "Violet"
-};
-int numberOfMajorColors =
-    sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
-const char* MinorColorNames[] = {
-    "Blue", "Orange", "Green", "Brown", "Slate"
-};
+const char* MajorColorNames[] = {"White", "Red", "Black", "Yellow", "Violet"};
+const char* MinorColorNames[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
+int numberOfMajorColors = sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
+int numberOfMinorColors = sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
 const int MAX_COLORPAIR_NAME_CHARS = 16;
-int numberOfMinorColors =
-    sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
 
 /***********Function definition**********************/
 
@@ -61,4 +55,28 @@ void testPairToNumber(
     int pairNumber = GetPairNumberFromColor(&colorPair);
     printf("Got pair number %d\n", pairNumber);
     assert(pairNumber == expectedPairNumber);
+}
+
+void printReference(void)
+{
+
+int majorCount = 0;
+int minorCount = 0;
+ColorPair colorPair;
+
+printf("==================color coding reference manual====================\n");
+printf("Pair No.\t\tmajor color\t\tminor color\n");
+for(; majorCount<=numberOfMajorColors; majorCount++)
+{
+	colorPair.majorColor = majorCount;
+	for(; minorCount<=numberOfMinorColors; minorCount++)
+	{
+		coloPair.minorColor = minorCount;
+		printf("%d\t\t",GetPairNumberFromColor(&colorPair));
+		printf("%s\t\t",MajorColorNames[majorCount]);
+		printf("%s\t\t\n",MinorColorNames[minorCount]);
+
+	}
+}
+
 }
